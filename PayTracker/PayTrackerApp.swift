4a41bda5fee2643f8e1ed8cfd9c7085db0199de1
bmtech <35602482+bmtechua@users.noreply.github.com>
2026-01-01 13:54:26 +0200

    Initial Commit

---
+++ PayTracker/PayTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayTrackerApp.swift
+//  PayTracker
+//
+//  Created by user on 01.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PayTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
